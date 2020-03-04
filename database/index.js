772a8827c98299503f4ff910314a3b9bcfa44a7d
John Balbin <jbalbin3@yahoo.com>
2020-03-03 20:13:20 -0800

    more heroku config stuff

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const jobSchema = require('./schema.js');
 mongoose.set('useFindAndModify', false);
-const mongoUri = 'mongodb://localhost/jobs-app';
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/jobs-app';
 mongoose.Promise = global.Promise;
 
 const db = mongoose.createConnection(mongoUri, {
